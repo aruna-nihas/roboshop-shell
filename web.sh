c4535fa2c98fab40f517449c8b8a54b1cb975f75
aruna <arunadegala18@gmail.com>
2023-12-19 11:38:02 +0530

    web.sh

---
+++ web.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.daws76s.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi
+
+dnf install nginx -y
+
+VALIDATE $? "INstalling Nginx"
+
+systemctl enable nginx
+
+VALIDATE $? "Enabiling Nginx"
+
+systemctl start nginx
+
+VALIDATE $? "Starting Nginx"
